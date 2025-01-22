6884fd24bbd67c7f81a0c05de63c4ebf79a55a80
Dan Hughes <2237515+dan-hughes@users.noreply.github.com>
2025-01-22 17:46:20 +0000

    DSC_xDhcpServerReservation Part 2

--- tests/Unit/DSC_xDhcpServerReservation.Tests.ps1
+++ tests/Unit/DSC_xDhcpServerReservation.Tests.ps1
@@ -156,7 +156,15 @@ Describe 'DSC_xDhcpServerReservation\Get-TargetResource' -Tag 'Get' {
     #             return [System.Net.IPAddress]::Parse('192.168.1.0')
     #         }
 
-    #         Mock -CommandName Get-DhcpServerv4Scope -MockWith { Write-Error -Message 'Missing' }
+    #         Mock -CommandName Get-DhcpServerv4Scope -MockWith {
+    #             $PSCmdlet.WriteError([System.Management.Automation.ErrorRecord]::new(
+    #                     'MockError',
+    #                     'ERR001',
+    #                     [System.Management.Automation.ErrorCategory]::InvalidOperation,
+    #                     $ScopeID
+    #                 )
+    #             )
+    #         }
     #     }
 
     #     It 'Should return the correct result' {
@@ -177,4 +185,384 @@ Describe 'DSC_xDhcpServerReservation\Get-TargetResource' -Tag 'Get' {
     #         }
     #     }
     # }
+}
+
+Describe 'DSC_xDhcpServerReservation\Set-TargetResource' -Tag 'Set' {
+    BeforeAll {
+        Mock -CommandName Update-ResourceProperties
+    }
+
+    Context 'When supplying parameters that are removed' {
+        It 'Should call the correct mocks' {
+            InModuleScope -ScriptBlock {
+                Set-StrictMode -Version 1.0
+
+                $mockParams = @{
+                    ScopeID          = '192.168.1.0'
+                    ClientMACAddress = '00-15-5D-01-05-1B'
+                    IPAddress        = '192.168.1.10'
+                    AddressFamily    = 'IPv4'
+                    Debug            = $true
+                }
+
+                Set-TargetResource @mockParams
+            }
+
+            Should -Invoke -CommandName Update-ResourceProperties -Exactly -Times 1 -Scope It
+        }
+    }
+}
+
+Describe 'DSC_xDhcpServerReservation\Test-TargetResource' -Tag 'Test' {
+    Context 'When the resource is in the desired state' {
+        BeforeAll {
+            Mock -CommandName Assert-Module
+            Mock -CommandName Get-ValidIPAddress -ParameterFilter {
+                $ParameterName -eq 'ScopeID'
+            } -MockWith {
+                return [System.Net.IPAddress]::Parse('192.168.1.0')
+            }
+
+            Mock -CommandName Get-DhcpServerv4Scope
+            Mock -CommandName Get-ValidIPAddress -ParameterFilter {
+                $ParameterName -eq 'IPAddress'
+            } -MockWith {
+                return [System.Net.IPAddress]::Parse('192.168.1.30')
+            }
+
+            Mock -CommandName Update-ResourceProperties -MockWith {
+                return $true
+            }
+        }
+
+        It 'Should call the expected mocks' {
+            InModuleScope -ScriptBlock {
+                Set-StrictMode -Version 1.0
+
+                $mockParams = @{
+                    ScopeID          = '192.168.1.0'
+                    ClientMACAddress = '00-15-5D-01-05-1B'
+                    IPAddress        = '192.168.1.30'
+                    AddressFamily    = 'IPv4'
+                    Ensure           = 'Present'
+                    Debug            = $true
+                }
+
+                Test-TargetResource @mockParams
+            }
+
+            Should -Invoke -CommandName Assert-Module -Exactly -Times 1 -Scope It
+            Should -Invoke Get-ValidIPAddress -ParameterFilter {
+                $ParameterName -eq 'ScopeID'
+            } -Exactly -Times 1 -Scope It
+            Should -Invoke -CommandName Get-DhcpServerv4Scope -Exactly -Times 1 -Scope It
+            Should -Invoke -CommandName Get-ValidIPAddress -ParameterFilter {
+                $ParameterName -eq 'IPAddress'
+            } -Exactly -Times 1 -Scope It
+            Should -Invoke -CommandName Update-ResourceProperties -Exactly -Times 1 -Scope It
+        }
+    }
+
+    # TODO: test Get-DhcpServerv4Scope error
+}
+
+Describe 'DSC_xDhcpServerReservation\Update-ResourceProperties' -Tag 'Helper' {
+    Context 'When a reservation exists' {
+        BeforeAll {
+            Mock -CommandName Get-DhcpServerv4Reservation -MockWith {
+                return @{
+                    ClientId  = '00-15-5D-01-05-1B'
+                    Name      = Get-ComputerName
+                    IPAddress = '192.168.30.10'
+                }
+            }
+        }
+
+        Context 'When the parameter ''Apply'' is $false' {
+            Context 'When the resource is in the desired state' {
+                It 'Should return the correct result' {
+                    InModuleScope -ScriptBlock {
+                        Set-StrictMode -Version 1.0
+
+                        $mockParams = @{
+                            ScopeID          = '192.168.30.0'
+                            ClientMACAddress = '00155D01051B'
+                            IPAddress        = '192.168.30.10'
+                            Name             = Get-ComputerName
+                            AddressFamily    = 'IPv4'
+                            Ensure           = 'Present'
+                        }
+
+                        Update-ResourceProperties @mockParams | Should -BeTrue
+                    }
+
+                    Should -Invoke -CommandName Get-DhcpServerv4Reservation -Exactly -Times 1 -Scope It
+                }
+            }
+
+            Context 'When the resource is not in the desired state' {
+                BeforeDiscovery {
+                    $testCases = @(
+                        @{
+                            Parameter = 'ClientMACAddress'
+                            Value     = '55155D01051B'
+                        }
+                        @{
+                            Parameter = 'Name'
+                            Value     = 'AnotherName'
+                        }
+                    )
+                }
+
+                Context 'When the ''<Parameter>'' is different' -ForEach $testCases {
+                    It 'Should return the correct result' {
+                        InModuleScope -Parameters $_ -ScriptBlock {
+                            Set-StrictMode -Version 1.0
+
+                            $mockParams = @{
+                                ScopeID          = '192.168.30.0'
+                                ClientMACAddress = '00155D01051B'
+                                IPAddress        = '192.168.30.10'
+                                Name             = Get-ComputerName
+                                AddressFamily    = 'IPv4'
+                                Ensure           = 'Present'
+                            }
+
+                            $mockParams.$Parameter = $Value
+
+                            Update-ResourceProperties @mockParams | Should -BeFalse
+                        }
+
+                        Should -Invoke -CommandName Get-DhcpServerv4Reservation -Exactly -Times 1 -Scope It
+                    }
+                }
+
+                Context 'When the resource should be absent' {
+                    It 'Should return the correct result' {
+                        InModuleScope -ScriptBlock {
+                            Set-StrictMode -Version 1.0
+
+                            $mockParams = @{
+                                ScopeID          = '192.168.30.0'
+                                ClientMACAddress = '00155D01051B'
+                                IPAddress        = '192.168.30.10'
+                                Name             = Get-ComputerName
+                                AddressFamily    = 'IPv4'
+                                Ensure           = 'Absent'
+                            }
+
+                            Update-ResourceProperties @mockParams | Should -BeFalse
+                        }
+
+                        Should -Invoke -CommandName Get-DhcpServerv4Reservation -Exactly -Times 1 -Scope It
+                    }
+                }
+            }
+        }
+
+        Context 'When the parameter ''Apply'' is $true' {
+            Context 'When the resource is in the desired state' {
+                It 'Should return the correct result' {
+                    InModuleScope -ScriptBlock {
+                        Set-StrictMode -Version 1.0
+
+                        $mockParams = @{
+                            ScopeID          = '192.168.30.0'
+                            ClientMACAddress = '00155D01051B'
+                            IPAddress        = '192.168.30.10'
+                            Name             = Get-ComputerName
+                            AddressFamily    = 'IPv4'
+                            Ensure           = 'Present'
+                            Apply            = $true
+                        }
+
+                        Update-ResourceProperties @mockParams
+                    }
+
+                    Should -Invoke -CommandName Get-DhcpServerv4Reservation -Exactly -Times 1 -Scope It
+                }
+            }
+
+            Context 'When the resource is not in the desired state' {
+                BeforeDiscovery {
+                    $testCases = @(
+                        @{
+                            Parameter = 'ClientMACAddress'
+                            Value     = '55155D01051B'
+                        }
+                        @{
+                            Parameter = 'Name'
+                            Value     = 'AnotherName'
+                        }
+                    )
+                }
+
+                Context 'When the ''<Parameter>'' is different' -ForEach $testCases {
+                    BeforeAll {
+                        Mock -CommandName Set-DhcpServerv4Reservation
+                        Mock -CommandName Write-PropertyMessage
+                    }
+
+                    It 'Should return the correct result' {
+                        InModuleScope -Parameters $_ -ScriptBlock {
+                            Set-StrictMode -Version 1.0
+
+                            $mockParams = @{
+                                ScopeID          = '192.168.30.0'
+                                ClientMACAddress = '00155D01051B'
+                                IPAddress        = '192.168.30.10'
+                                Name             = Get-ComputerName
+                                AddressFamily    = 'IPv4'
+                                Ensure           = 'Present'
+                                Apply            = $true
+                            }
+
+                            $mockParams.$Parameter = $Value
+
+                            Update-ResourceProperties @mockParams
+                        }
+
+                        Should -Invoke -CommandName Get-DhcpServerv4Reservation -Exactly -Times 1 -Scope It
+                        Should -Invoke -CommandName Set-DhcpServerv4Reservation -Exactly -Times 1 -Scope It
+                        Should -Invoke -CommandName Write-PropertyMessage -Exactly -Times 1 -Scope It
+                    }
+                }
+
+                Context 'When the resource should be removed' {
+                    BeforeAll {
+                        Mock -CommandName Remove-DhcpServerv4Reservation
+                    }
+
+                    It 'Should call the correct mocks' {
+                        InModuleScope -ScriptBlock {
+                            Set-StrictMode -Version 1.0
+
+                            $mockParams = @{
+                                ScopeID          = '192.168.30.0'
+                                ClientMACAddress = '00155D01051B'
+                                IPAddress        = '192.168.30.10'
+                                Name             = Get-ComputerName
+                                AddressFamily    = 'IPv4'
+                                Ensure           = 'Absent'
+                                Apply            = $true
+                            }
+
+                            Update-ResourceProperties @mockParams
+                        }
+
+                        Should -Invoke -CommandName Get-DhcpServerv4Reservation -Exactly -Times 1 -Scope It
+                        Should -Invoke -CommandName Remove-DhcpServerv4Reservation -Exactly -Times 1 -Scope It
+                    }
+                }
+            }
+        }
+    }
+
+    Context 'When a reservation does not exist' {
+        BeforeAll {
+            Mock -CommandName Get-DhcpServerv4Reservation
+        }
+
+        Context 'When ''Apply'' is $false' {
+            Context 'When a reservation should exist' {
+                It 'Should return the correct result' {
+                    InModuleScope -ScriptBlock {
+                        Set-StrictMode -Version 1.0
+
+                        $mockParams = @{
+                            ScopeID          = '192.168.30.0'
+                            ClientMACAddress = '00155D01051B'
+                            IPAddress        = '192.168.30.10'
+                            Name             = Get-ComputerName
+                            AddressFamily    = 'IPv4'
+                            Ensure           = 'Present'
+                        }
+
+                        Update-ResourceProperties @mockParams | Should -BeFalse
+                    }
+
+                    Should -Invoke -CommandName Get-DhcpServerv4Reservation -Exactly -Times 1 -Scope It
+                }
+            }
+
+            Context 'When a reservation should not exist' {
+                It 'Should return the correct result' {
+                    InModuleScope -ScriptBlock {
+                        Set-StrictMode -Version 1.0
+
+                        $mockParams = @{
+                            ScopeID          = '192.168.30.0'
+                            ClientMACAddress = '00155D01051B'
+                            IPAddress        = '192.168.30.10'
+                            Name             = Get-ComputerName
+                            AddressFamily    = 'IPv4'
+                            Ensure           = 'Absent'
+                        }
+
+                        Update-ResourceProperties @mockParams | Should -BeTrue
+                    }
+
+                    Should -Invoke -CommandName Get-DhcpServerv4Reservation -Exactly -Times 1 -Scope It
+                }
+            }
+        }
+
+        Context 'When ''Apply'' is $true' {
+            Context 'When a reservation should exist' {
+                Context 'When the reservation is successfully created' {
+                    BeforeAll {
+                        Mock -CommandName Add-DhcpServerv4Reservation
+                    }
+
+                    It 'Should call the correct mocks' {
+                        InModuleScope -ScriptBlock {
+                            Set-StrictMode -Version 1.0
+
+                            $mockParams = @{
+                                ScopeID          = '192.168.30.0'
+                                ClientMACAddress = '00155D01051B'
+                                IPAddress        = '192.168.30.10'
+                                Name             = Get-ComputerName
+                                AddressFamily    = 'IPv4'
+                                Ensure           = 'Present'
+                                Apply            = $true
+                            }
+
+                            Update-ResourceProperties @mockParams
+                        }
+
+                        Should -Invoke -CommandName Get-DhcpServerv4Reservation -Exactly -Times 1 -Scope It
+                        Should -Invoke -CommandName Add-DhcpServerv4Reservation -Exactly -Times 1 -Scope It
+                    }
+                }
+
+                Context 'When the reservation fails to be created' {
+                    BeforeAll {
+                        Mock -CommandName Add-DhcpServerv4Reservation -MockWith { throw }
+                    }
+
+                    It 'Should throw and exception' {
+                        InModuleScope -ScriptBlock {
+                            Set-StrictMode -Version 1.0
+
+                            $mockParams = @{
+                                ScopeID          = '192.168.30.0'
+                                ClientMACAddress = '00155D01051B'
+                                IPAddress        = '192.168.30.10'
+                                Name             = Get-ComputerName
+                                AddressFamily    = 'IPv4'
+                                Ensure           = 'Present'
+                                Apply            = $true
+                            }
+
+                            { Update-ResourceProperties @mockParams } | Should -Throw
+                        }
+
+                        Should -Invoke -CommandName Get-DhcpServerv4Reservation -Exactly -Times 1 -Scope It
+                        Should -Invoke -CommandName Add-DhcpServerv4Reservation -Exactly -Times 1 -Scope It
+                    }
+                }
+            }
+        }
+    }
 }
